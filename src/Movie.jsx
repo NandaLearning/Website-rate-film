5fc420897bd3a8368194997f351bc920a0ce5b3c
nanda <anandalukman01@gmail.com>
2023-09-10 00:25:28 -0700

    first

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Navigasi from "./components/Navigasi";
+import Footer from "./components/Footer";
 
 export default function Movie() {
   const [movieList, setMovieList] = useState([]);
@@ -20,14 +21,14 @@ export default function Movie() {
     <div className="flex flex-wrap justify-center">
       <Navigasi/>
       {movieList.map((movie) => (
-        <div key={movie.id} className="max-w-sm m-2 mt-10 w-64 ml-12">
+        <div key={movie.id} className="max-w-sm m-2 mt-10 w-64 ml-5">
           <div className="bg-white shadow-lg rounded-lg overflow-hidden transition-transform hover:scale-125 duration-200">
             <img
               className="w-full h-96"
               src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
               alt={movie.title}
             />
-            <div className="p-4">
+            <div className="p-2">
               <h2 className="text-xl font-semibold text-black">{movie.title}</h2>
               <p className="text-black">Release Date: {movie.release_date}</p>
               <p className="text-black">Vote Average: {movie.vote_average}</p>
@@ -35,6 +36,9 @@ export default function Movie() {
           </div>
         </div>
       ))}
+  
+
+  <Footer/>
     </div>
   );
 }
--- src/components/Navigasi.jsx
+++ src/components/Navigasi.jsx
@@ -3,7 +3,14 @@ export default function Navigasi () {
         <div className=" bg-white text-black w-full h-20 drop-shadow-xl">
             <div className=" justify-center items-center flex">
             <a href="/" className=" font-bold hover:text-blue-500 duration-200 text-2xl mt-5">NandooFilm</a>
+
+           
             </div>
+
+            
+
+           
+       
 
         </div>
     )
